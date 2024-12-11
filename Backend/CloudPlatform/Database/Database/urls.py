6b72761ab211c0a077251fa358469d1c33c690ac
sainanduk <sainandupatel6888@gmail.com>
2024-12-11 20:27:30 +0530

    setup db done

--- Backend/CloudPlatform/Database/Database/urls.py
+++ Backend/CloudPlatform/Database/Database/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from DB.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/',hello ),
 ]
